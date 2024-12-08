f60dc203adcc79a94c9d2dc4674d6da39606932f
galahadtrail <zzzzrzzzz@yandex.ru>
2024-12-08 12:37:22 +0300

    added path changing

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ use cheching::{matching_rules, write_hash_rules_from_file};
 use connection::connect;
 use greetings::print_hello_message;
 
+use colored::*;
 use notify::event::{CreateKind, ModifyKind};
 use notify::{recommended_watcher, Event, EventKind, RecursiveMode, Result, Watcher};
+use std::io;
 use std::path::Path;
 use std::sync::mpsc;
 
@@ -21,14 +23,23 @@ fn main() -> Result<()> {
         .collect();
     let rules = new_rules.clone();
     let _ = write_hash_rules_from_file("src/rules/rules.txt", new_rules);
+
+    let mut alerts: Vec<String> = Vec::new();
     // Создаем канал для получения событий
     let (tx, rx) = mpsc::channel::<Result<Event>>();
 
     // Создаем объект watcher с задержкой 2 секунды
     let mut watcher = recommended_watcher(tx)?;
 
+    let mut path = String::new();
+    let write = "Enter the path where you want to track files:"
+        .truecolor(193, 251, 222)
+        .on_purple();
+    println!("{}", write);
+    let _ = io::stdin().read_line(&mut path);
+
     // Указываем директорию для отслеживания
-    watcher.watch(Path::new("."), RecursiveMode::NonRecursive)?;
+    watcher.watch(Path::new(&path), RecursiveMode::NonRecursive)?;
 
     for res in rx {
         match res {
@@ -39,6 +50,9 @@ fn main() -> Result<()> {
                 {
                     for path in event.paths.iter() {
                         let math_res = matching_rules(&rules, path.to_path_buf());
+                        if math_res {
+                            alerts.push(String::from(path.to_str().unwrap()));
+                        }
                     }
                 }
             }
